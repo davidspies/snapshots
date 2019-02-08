3a80ac78937ab7331e7fc5e3ac21de2d291fc63c
David Spies <dnspies@gmail.com>
2019-02-08 09:51:35 -0800

    Get the name right

--- stacknew.py
+++ stacknew.py
@@ -9,7 +9,7 @@ tmpl_url = (
     )
 resolver = (
     'https://raw.githubusercontent.com/davidspies/snapshots/master/' +
-    'dspies-snapshot-2.yaml'
+    'dspies-snapshot-4.yaml'
     )
 
 def main(loc, *args):
