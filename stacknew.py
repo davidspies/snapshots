1943642c9266b062703a8a613dc4e143857b4611
David Spies <dnspies@gmail.com>
2019-02-08 09:42:12 -0800

    Nix doesn't work. Forget it

--- stacknew.py
+++ stacknew.py
@@ -7,10 +7,13 @@ tmpl_url = (
     "https://raw.githubusercontent.com/davidspies/snapshots/master/"
     + "dspies.hsfiles"
     )
-
+resolver = (
+    'https://raw.githubusercontent.com/davidspies/snapshots/master/' +
+    'dspies-snapshot-2.yaml'
+    )
 
 def main(loc, *args):
-    os.execvp("stack", ("stack", "new", loc, tmpl_url) + args)
+    os.execvp("stack", ("stack", "new", loc, tmpl_url, '--resolver=' + resolver) + args)
 
 
 if __name__ == '__main__':
