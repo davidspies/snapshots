2d1a5f9201ce6f536f105915b52ee17f963c7b75
David Spies <dnspies@gmail.com>
2019-02-24 12:38:15 -0800

    Better error combining

--- stacknew.py
+++ stacknew.py
@@ -9,7 +9,7 @@ tmpl_url = (
     )
 resolver = (
     'https://raw.githubusercontent.com/davidspies/snapshots/master/' +
-    'dspies-snapshot-9.yaml'
+    'dspies-snapshot-10.yaml'
     )
 
 def main(loc, *args):
