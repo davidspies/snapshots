6ebca2c78cb24b4d93761411fd32a19d68c89861
David Spies <dnspies@gmail.com>
2019-03-05 00:47:01 -0800

    Update hie

--- stacknew.py
+++ stacknew.py
@@ -9,7 +9,7 @@ tmpl_url = (
     )
 resolver = (
     'https://raw.githubusercontent.com/davidspies/snapshots/master/' +
-    'dspies-snapshot-11.yaml'
+    'dspies-snapshot-12.yaml'
     )
 
 def main(loc, *args):
